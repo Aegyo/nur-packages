5fe3bd50b33e7585fb0fbb2b089ee832569ff4b1
Jessica Chen <jessica.scoltock@protonmail.com>
2023-05-28 21:02:35 +0100

    Add otrecorder

---
+++ pkgs/otrecorder/default.nix
@@ -0,0 +1,35 @@
+{ pkgs, lib, fetchFromGitHub, ... }:
+
+pkgs.stdenv.mkDerivation rec {
+  pname = "otrecorder";
+  name = pname;
+  version = "0.9.1";
+
+  src = fetchFromGitHub {
+    owner = "owntracks";
+    repo = "recorder";
+    rev = version;
+    sha256 = "sha256-/y74jfofvWTcHSX+9wtrCRclaS5Aw03TCz11mrZiqiM=";
+  };
+
+  nativeBuildInputs = with pkgs; [ mosquitto curl lmdb libconfig libuuid ];
+
+  configurePhase = ''
+    cp config.mk.in config.mk
+  '';
+
+  NIX_CFLAGS_COMPILE = "-Wno-error";
+
+  installPhase = ''
+    mkdir -p $out/bin $out/usr/share/ot-recorder
+    cp ot-recorder $out/bin/ot-recorder
+    cp ocat $out/bin/ocat
+    cp -R docroot/* $out/usr/share/ot-recorder
+  '';
+
+  meta = with lib; {
+    description = "Store and access data published by OwnTracks apps";
+    homepage = "https://github.com/owntracks/recorder";
+    license = licenses.gpl2;
+  };
+}
